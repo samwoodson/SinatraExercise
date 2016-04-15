56c504126344d49e8b61150a76a9fa93ec0f04bb
samwoodson <samuelwoodson@gmail.com>
2016-04-15 21:42:53 +0000

    post new message enabled, and message details

--- app/actions.rb
+++ app/actions.rb
@@ -7,3 +7,23 @@ get '/messages' do
   @messages = Message.all
   erb :'messages/index'
 end
+
+get '/messages/new' do
+  erb :'messages/new'  
+end
+
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]
+  )
+  @message.save
+  redirect '/messages'
+end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
+
